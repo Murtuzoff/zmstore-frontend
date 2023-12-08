78b8f542274983738f71303432fad77b56590731
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 16:38:18 +0300

    test

--- src/api/serverAPI.js
+++ src/api/serverAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userAPI = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/user`,
+  baseURL: `https://zmstore-backend.vercel.app/api/user`,
 });
 
 const productAPI = axios.create({
